a2cdc88955b691e25a14d49e791e89a3a28a3c29
Yeraygb <yeraygb96@gmail.com>
2022-05-21 22:08:08 +0200

    1 byte

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -39,11 +39,11 @@ char	*ft_strjoin(char *s1, char *s2)
 	int		i;
 	int		n;
 
-	if (!s1)
+/* 	if (!s1)
 	{
 		s1 = (char *)malloc(1 * sizeof(char));
 		s1[0] = '\0';
-	}
+	} */
 	if (!s1 || !s2)
 		return (NULL);
 	str = malloc((ft_strlen(s1) + ft_strlen(s2) + 1) * sizeof(char));
