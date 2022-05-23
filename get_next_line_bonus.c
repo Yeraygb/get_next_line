de68e4dd0fe32205853b5eaccb44a926d5f79b07
Yeray Gonzalez bartolome <ygonzale@c3r3s6.42urduliz.com>
2022-05-23 13:09:58 +0200

    bonus 1 abort

---
+++ get_next_line_bonus.c
@@ -0,0 +1,138 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ygonzale <ygonzale@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/23 11:32:54 by ygonzale          #+#    #+#             */
+/*   Updated: 2022/05/23 12:55:09 by ygonzale         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line_bonus.h"
+
+char	*ft_read(int fd, char *savebuf)
+{
+	ssize_t	nr_bytes;
+	char	*buf;
+
+	buf = malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	if (!buf)
+		return (NULL);
+	nr_bytes = 1;
+	while (!ft_strchr(savebuf, '\n') && nr_bytes != 0)
+	{
+		nr_bytes = read(fd, buf, BUFFER_SIZE);
+		if (nr_bytes == -1)
+		{
+			free (buf);
+			return (NULL);
+		}
+		if (nr_bytes == 0)
+			break ;
+		buf[nr_bytes] = '\0';
+		savebuf = ft_strjoin(savebuf, buf);
+	}
+	free (buf);
+	return (savebuf);
+}
+
+int	line_jump(char *savebuf)
+{
+	int		i;
+
+	i = 0;
+	while (savebuf[i])
+	{
+		if (savebuf[i] == '\n')
+			return (++i);
+		i++;
+	}
+	return (i);
+}
+
+char	*ft_substr_line(char *savebuf)
+{
+	char	*str;
+	size_t	i;
+	size_t	n;
+
+	i = 0;
+	n = line_jump(savebuf);
+	if (!savebuf[i])
+		return (NULL);
+	str = (char *)malloc(sizeof(char) * (n + 2));
+	if (!str)
+		return (NULL);
+	while (savebuf[i] && savebuf[i] != '\n')
+	{
+		str[i] = savebuf[i];
+		i++;
+	}
+	if (savebuf[i] == '\n')
+	{
+		str[i] = savebuf[i];
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
+
+char	*ft_substr_static(char *s)
+{
+	char	*str;
+	size_t	j;
+	size_t	n;
+
+	n = line_jump(s);
+	j = 0;
+	if (!s[n])
+	{
+		free (s);
+		return (NULL);
+	}
+	str = (char *)malloc(sizeof(char) * (ft_strlen(s) - n + 1));
+	if (!str)
+		return (NULL);
+	while (s[n])
+		str[j++] = s[n++];
+	str[j] = '\0';
+	free (s);
+	return (str);
+}
+
+char	*get_next_line(int fd)
+{
+	char			*line;
+	static char		*savebuf[4096];
+
+	if (savebuf[fd] < 0 || BUFFER_SIZE <= 0 || fd >= 4097)
+		return (NULL);
+	savebuf[fd] = ft_read(fd, savebuf[fd]);
+	if (!savebuf[fd])
+	{
+		free (savebuf[fd]);
+		return (NULL);
+	}
+	line = ft_substr_line(savebuf[fd]);
+	savebuf[fd] = ft_substr_static(savebuf[fd]);
+	return (line);
+}
+
+/* int main()
+{
+	ssize_t	fd = open("test1.txt", O_RDONLY);
+	size_t	i = 3;
+	char	*line;
+
+	while (i--)
+	{
+		line = get_next_line(fd);
+		printf("line %zu = %s", i, line);
+		free(line);
+	}
+	close(fd);
+	return (0);
+}
+ */
--- get_next_line_utils_bonus.c
+++ get_next_line_utils_bonus.c
@@ -1,16 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*   get_next_line_utils_bonus.c                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ygonzale <ygonzale@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/05/04 12:44:29 by ygonzale          #+#    #+#             */
-/*   Updated: 2022/05/23 11:35:19 by ygonzale         ###   ########.fr       */
+/*   Created: 2022/05/23 11:35:29 by ygonzale          #+#    #+#             */
+/*   Updated: 2022/05/23 11:53:17 by ygonzale         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
+#include "get_next_line_bonus.h"
 
 size_t	ft_strlen(const char *str)
 {
